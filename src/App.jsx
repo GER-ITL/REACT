53372447f880fc1b350b97fe8606c22d35dec728
GER-ITL <danteryzvelt@inbox.ru>
2023-09-18 13:13:40 +0300

    react

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <h1 
+        className="main"
+        
+        >
+          KAZAXSTAN
+        </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
